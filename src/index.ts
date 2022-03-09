746b595e6744e1b53ce41d957c3c7fe8e3cb2d20
David Vargas <dvargas92495@gmail.com>
2022-03-08 22:35:45 -0600

    Migrated from mono

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,49 @@
-import toConfigPageName from "roamjs-components/util/toConfigPageName";
+import createButtonObserver from "roamjs-components/dom/createButtonObserver";
+import createHTMLObserver from "roamjs-components/dom/createHTMLObserver";
+import getTextByBlockUid from "roamjs-components/queries/getTextByBlockUid";
+import getUidsFromId from "roamjs-components/dom/getUidsFromId";
+import { renderQueryBuilder } from "./components/QueryBuilder";
 import runExtension from "roamjs-components/util/runExtension";
-import { createConfigObserver } from "roamjs-components/components/ConfigPage";
 
-const ID = "query-builder";
-const CONFIG = toConfigPageName(ID);
-runExtension(ID, () => {
-  createConfigObserver({ title: CONFIG, config: { tabs: [] } });
+runExtension("query-builder", () => {
+  const css = document.createElement("style");
+  css.textContent = `.bp3-button:focus {
+    outline-width: 2px;
+}`;
+  document.getElementsByTagName("head")[0].appendChild(css);
+
+  createButtonObserver({
+    shortcut: "qb",
+    attribute: "query-builder",
+    render: (b: HTMLButtonElement) =>
+      renderQueryBuilder({
+        blockId: b.closest(".roam-block").id,
+        parent: b.parentElement,
+      }),
+  });
+
+  const dataAttribute = "data-roamjs-edit-query";
+
+  createHTMLObserver({
+    callback: (b) => {
+      if (!b.getAttribute(dataAttribute)) {
+        b.setAttribute(dataAttribute, "true");
+        const editButtonRoot = document.createElement("div");
+        b.appendChild(editButtonRoot);
+        const blockId = b.closest(".roam-block").id;
+        const initialValue = getTextByBlockUid(getUidsFromId(blockId).blockUid);
+        renderQueryBuilder({
+          blockId,
+          parent: editButtonRoot,
+          initialValue,
+        });
+        const editButton = document.getElementById(
+          `roamjs-query-builder-button-${blockId}`
+        );
+        editButton.addEventListener("mousedown", (e) => e.stopPropagation());
+      }
+    },
+    tag: "DIV",
+    className: "rm-query-title",
+  });
 });
