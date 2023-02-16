b27223caccb6da54007d4e2913f46508e74235b8
Michael Gartner <mclicks@gmail.com>
2023-02-15 17:04:08 -0800

    SB command check for alias or page ref

--- src/index.ts
+++ src/index.ts
@@ -422,8 +422,20 @@ export default runExtension({
       handler:
         ({ proccessBlockText, variables }) =>
         (arg, format = "(({uid}))") => {
-          const queryUid = variables[arg] || arg;
-          const parentUid = extractRef(queryUid);
+          const queryRef = variables[arg] || arg;
+          const aliasOrPage = window.roamAlphaAPI.data.fast.q(
+            `[:find ?uid :where [?b :block/uid ?uid] [or-join [?b] 
+                 [and [?b :block/string ?s] [[clojure.string/includes? ?s "{{query block:${queryRef}}}"]] ]
+                 ${getQueryPages(extensionAPI).map(
+                   (p) =>
+                     `[and [?b :node/title ?t] [[re-pattern "^${p.replace(
+                       /\*/,
+                       queryRef
+                     )}$"] ?regex] [[re-find ?regex ?t]]]`
+                 )}
+            ]]`
+          )[0]?.toString();
+          const parentUid = aliasOrPage || extractRef(queryRef);
           return runQuery(parentUid, extensionAPI).then(({ allResults }) => {
             return allResults
               .map((r) =>
