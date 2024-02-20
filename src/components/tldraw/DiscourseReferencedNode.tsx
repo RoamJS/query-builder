6465ce5247e3b89a4937d868f4e0f3e88db3ae21
Michael Gartner <mclicks@gmail.com>
2024-02-19 23:02:26 -0600

    combine ReferencedNode and DiscourseRelation Utils

--- src/components/tldraw/DiscourseReferencedNode.tsx
+++ src/components/tldraw/DiscourseReferencedNode.tsx
@@ -1,7 +1,3 @@
-// Helper to add referenced nodes to node titles
-// EG: [[EVD]] - {content} - {Source}
-// {Source} is a referenced node
-
 import {
   TLArrowShape,
   ArrowShapeUtil,
@@ -35,115 +31,71 @@ export const createRelationShapeTools = (allRelationNames: string[]) => {
 
 export const createRelationShapeUtils = (relationIds: string[]) => {
   return relationIds.map((id) => {
-    class DiscourseRelationUtil extends DiscourseBaseRelationUtil {
+    class DiscourseRelationUtil extends ArrowShapeUtil {
       static type = id;
+      override canBind = () => true;
+      override canEdit = () => false;
+      defaultProps() {
+        const relations = Object.values(discourseContext.relations);
+        // TODO - add canvas settings to relations config
+        const relationIndex = relations.findIndex((rs) =>
+          rs.some((r) => r.id === this.type)
+        );
+        const isValid = relationIndex >= 0 && relationIndex < relations.length;
+        const color = isValid ? COLOR_ARRAY[relationIndex + 1] : COLOR_ARRAY[0];
+        return {
+          opacity: "1" as const,
+          dash: "draw" as const,
+          size: "s" as const,
+          fill: "none" as const,
+          color,
+          labelColor: color,
+          bend: 0,
+          start: { type: "point" as const, x: 0, y: 0 },
+          end: { type: "point" as const, x: 0, y: 0 },
+          arrowheadStart: "none" as const,
+          arrowheadEnd: "arrow" as const,
+          text: isValid
+            ? Object.keys(discourseContext.relations)[relationIndex]
+            : "",
+          font: "mono" as const,
+        };
+      }
+      override onBeforeCreate = (shape: TLArrowShape) => {
+        // TODO - propsForNextShape is clobbering our choice of color
+        const relations = Object.values(discourseContext.relations);
+        const relationIndex = relations.findIndex((rs) =>
+          rs.some((r) => r.id === this.type)
+        );
+        const isValid = relationIndex >= 0 && relationIndex < relations.length;
+        const color = isValid ? COLOR_ARRAY[relationIndex + 1] : COLOR_ARRAY[0];
+        return {
+          ...shape,
+          props: {
+            ...shape.props,
+            color,
+            labelColor: color,
+          },
+        };
+      };
+      component(shape: TLArrowShape) {
+        return (
+          <>
+            <style>{`#${shape.id.replace(":", "_")}_clip_0 {
+  display: none;
+}
+[data-shape-type="${this.type}"] .rs-arrow-label {
+  left: 0;
+  top: 0;
+  width: unset;
+  height: unset;
+}
+`}</style>
+            {super.component(shape)}
+          </>
+        );
+      }
     }
     return DiscourseRelationUtil;
   });
 };
-
-class DiscourseReferencedNodeUtil extends ArrowShapeUtil {
-  type = "placeholder";
-  override canBind = () => true;
-  override canEdit = () => false;
-  defaultProps() {
-    return {
-      opacity: "1" as const,
-      dash: "draw" as const,
-      size: "s" as const,
-      fill: "none" as const,
-      color: COLOR_ARRAY[0],
-      labelColor: COLOR_ARRAY[1],
-      bend: 0,
-      start: { type: "point" as const, x: 0, y: 0 },
-      end: { type: "point" as const, x: 0, y: 0 },
-      arrowheadStart: "none" as const,
-      arrowheadEnd: "arrow" as const,
-      text: "for",
-      font: "mono" as const,
-    };
-  }
-  component(shape: TLArrowShape) {
-    return (
-      <>
-        <style>{`#${shape.id.replace(":", "_")}_clip_0 {
-  display: none;
-}
-[data-shape-type="${this.type}"] .rs-arrow-label {
-  left: 0;
-  top: 0;
-  width: unset;
-  height: unset;
-}
-`}</style>
-        {super.component(shape)}
-      </>
-    );
-  }
-}
-export class DiscourseBaseRelationUtil extends ArrowShapeUtil {
-  static type = "placeholder";
-
-  override canBind = () => true;
-  override canEdit = () => false;
-  defaultProps() {
-    const relations = Object.values(discourseContext.relations);
-    // TODO - add canvas settings to relations config
-    const relationIndex = relations.findIndex((rs) =>
-      rs.some((r) => r.id === this.type)
-    );
-    const isValid = relationIndex >= 0 && relationIndex < relations.length;
-    const color = isValid ? COLOR_ARRAY[relationIndex + 1] : COLOR_ARRAY[0];
-    return {
-      opacity: "1" as const,
-      dash: "draw" as const,
-      size: "s" as const,
-      fill: "none" as const,
-      color,
-      labelColor: color,
-      bend: 0,
-      start: { type: "point" as const, x: 0, y: 0 },
-      end: { type: "point" as const, x: 0, y: 0 },
-      arrowheadStart: "none" as const,
-      arrowheadEnd: "arrow" as const,
-      text: isValid
-        ? Object.keys(discourseContext.relations)[relationIndex]
-        : "",
-      font: "mono" as const,
-    };
-  }
-  override onBeforeCreate = (shape: TLArrowShape) => {
-    // TODO - propsForNextShape is clobbering our choice of color
-    const relations = Object.values(discourseContext.relations);
-    const relationIndex = relations.findIndex((rs) =>
-      rs.some((r) => r.id === this.type)
-    );
-    const isValid = relationIndex >= 0 && relationIndex < relations.length;
-    const color = isValid ? COLOR_ARRAY[relationIndex + 1] : COLOR_ARRAY[0];
-    return {
-      ...shape,
-      props: {
-        ...shape.props,
-        color,
-        labelColor: color,
-      },
-    };
-  };
-  component(shape: TLArrowShape) {
-    return (
-      <>
-        <style>{`#${shape.id.replace(":", "_")}_clip_0 {
-  display: none;
-}
-[data-shape-type="${this.type}"] .rs-arrow-label {
-  left: 0;
-  top: 0;
-  width: unset;
-  height: unset;
-}
-`}</style>
-        {super.component(shape)}
-      </>
-    );
-  }
-}
