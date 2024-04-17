e48534801ae14dc64c35745e4d533563e6ddab37
Michael Gartner <mclicks@gmail.com>
2024-04-17 13:31:05 -0600

    add github destination

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -43,6 +43,9 @@ import { Column } from "../utils/types";
 import { render as renderToast } from "roamjs-components/components/Toast";
 import { getNodeEnv } from "roamjs-components/util/env";
 import apiGet from "roamjs-components/util/apiGet";
+import apiPut from "roamjs-components/util/apiPut";
+import localStorageGet from "roamjs-components/util/localStorageGet";
+import { ExportGithub } from "./ExportGithub";
 
 const ExportProgress = ({ id }: { id: string }) => {
   const [progress, setProgress] = useState(0);
@@ -91,6 +94,7 @@ const EXPORT_DESTINATIONS = [
   { id: "local", label: "Download Locally", active: true },
   { id: "app", label: "Store in Roam", active: false },
   { id: "samepage", label: "Store with SamePage", active: false },
+  { id: "github", label: "Send to GitHub", active: true },
 ];
 const exportDestinationById = Object.fromEntries(
   EXPORT_DESTINATIONS.map((ed) => [ed.id, ed])
@@ -105,6 +109,9 @@ const ExportDialog: ExportDialogComponent = ({
   isExportDiscourseGraph = false,
   initialPanel,
 }) => {
+  const [selectedRepo, setSelectedRepo] = useState(
+    localStorageGet("selected-repo")
+  );
   const exportId = useMemo(() => nanoid(), []);
   useEffect(() => {
     setDialogOpen(isOpen);
@@ -158,6 +165,47 @@ const ExportDialog: ExportDialogComponent = ({
   const [includeDiscourseContext, setIncludeDiscourseContext] = useState(
     discourseGraphEnabled as boolean
   );
+  const [gitHubAccessToken, setGitHubAccessToken] = useState<string | null>(
+    localStorageGet("oauth-github")
+  );
+  const [clearGitHubToken, setClearGitHubToken] = useState(false);
+  const [canSendToGitHub, setCanSendToGitHub] = useState(false);
+
+  const writeFileToRepo = async ({
+    filename,
+    content,
+    setError,
+  }: {
+    filename: string;
+    content: string;
+    setError: (error: string) => void;
+  }): Promise<{ status: number }> => {
+    const base64Content = btoa(content);
+
+    try {
+      const response = await apiPut({
+        domain: "https://api.github.com",
+        path: `repos/${selectedRepo}/contents/${filename}`,
+        headers: {
+          Authorization: `token ${gitHubAccessToken}`,
+        },
+        data: {
+          message: `Add ${filename}`,
+          content: base64Content,
+        },
+      });
+      if (response.status === 401) {
+        setError("Authentication failed. Please log in again.");
+        setClearGitHubToken(true);
+        return { status: 401 };
+      }
+      return { status: response.status };
+    } catch (error) {
+      setError("Failed to upload file to repo");
+      return { status: 500 };
+    }
+  };
+
   const handleSetSelectedPage = (title: string) => {
     setSelectedPageTitle(title);
     setSelectedPageUid(getPageUidByPageTitle(title));
@@ -414,7 +462,7 @@ const ExportDialog: ExportDialogComponent = ({
   const ExportPanel = (
     <>
       <div className={Classes.DIALOG_BODY}>
-        <div className="flex justify-between items-center gap-16">
+        <div className="flex justify-between items-start gap-16">
           <Label className="flex-grow">
             Export Type
             <MenuItemSelect
@@ -423,21 +471,41 @@ const ExportDialog: ExportDialogComponent = ({
               onItemSelect={(et) => setActiveExportType(et)}
             />
           </Label>
-          <Label className="flex-grow">
-            Destination
-            <MenuItemSelect
-              items={EXPORT_DESTINATIONS.map((ed) => ed.id)}
-              transformItem={(s) => exportDestinationById[s].label}
-              activeItem={activeExportDestination}
-              onItemSelect={(et) => setActiveExportDestination(et)}
+          <div>
+            <Label className="flex-grow">
+              Destination
+              <MenuItemSelect
+                items={
+                  results.length === 1 && activeExportType === "Markdown" // TODO handle more github exports
+                    ? EXPORT_DESTINATIONS.map((ed) => ed.id)
+                    : EXPORT_DESTINATIONS.map((ed) => ed.id).filter(
+                        (ed) => ed !== "github"
+                      )
+                }
+                transformItem={(s) => exportDestinationById[s].label}
+                activeItem={activeExportDestination}
+                onItemSelect={(et) => setActiveExportDestination(et)}
+              />
+            </Label>
+            <ExportGithub
+              isVisible={activeExportDestination === "github"}
+              selectedRepo={selectedRepo}
+              setSelectedRepo={setSelectedRepo}
+              setError={setError}
+              clearGitHubToken={clearGitHubToken}
+              gitHubAccessToken={gitHubAccessToken}
+              setGitHubAccessToken={setGitHubAccessToken}
+              setCanSendToGitHub={setCanSendToGitHub}
             />
-          </Label>
+          </div>
         </div>
+
         <Label>
           Filename
           <InputGroup
             value={filename}
             onChange={(e) => setFilename(e.target.value)}
+            disabled={results.length === 1} // TODO handle single result filename based on export type or user input
           />
         </Label>
 
@@ -547,6 +615,30 @@ const ExportDialog: ExportDialogComponent = ({
                       return;
                     }
 
+                    if (activeExportDestination === "github") {
+                      const { title, content } = files[0];
+                      try {
+                        const { status } = await writeFileToRepo({
+                          filename: title,
+                          content,
+                          setError,
+                        });
+                        if (status === 201) {
+                          // TODO: remove toast by prolonging ExportProgress
+                          renderToast({
+                            id: "export-success",
+                            content: "Upload Success",
+                            intent: "success",
+                          });
+                          onClose();
+                        }
+                      } catch (error) {
+                        const e = error as Error;
+                        setError(e.message);
+                      }
+                      return;
+                    }
+
                     if (files.length === 1) {
                       const { title, content } = files[0];
                       const blob = new Blob([content], {
@@ -602,7 +694,10 @@ const ExportDialog: ExportDialogComponent = ({
               }, 1);
             }}
             style={{ minWidth: 64 }}
-            disabled={loading}
+            disabled={
+              loading ||
+              (activeExportDestination === "github" && !canSendToGitHub)
+            }
           />
         </div>
       </div>
---
+++ src/components/ExportGithub.tsx
@@ -0,0 +1,211 @@
+import { Button } from "@blueprintjs/core";
+import React, { useEffect, useRef, useState } from "react";
+import MenuItemSelect from "roamjs-components/components/MenuItemSelect";
+import apiGet from "roamjs-components/util/apiGet";
+import localStorageSet from "roamjs-components/util/localStorageSet";
+
+type UserReposResponse = {
+  data: [
+    {
+      name: string;
+      full_name: string;
+    }
+  ];
+  status: number;
+};
+type UserRepos = UserReposResponse["data"];
+const initialRepos: UserRepos = [{ name: "", full_name: "" }];
+
+const WINDOW_WIDTH = 600;
+const WINDOW_HEIGHT = 525;
+const WINDOW_LEFT = window.screenX + (window.innerWidth - WINDOW_WIDTH) / 2;
+const WINDOW_TOP = window.screenY + (window.innerHeight - WINDOW_HEIGHT) / 2;
+
+export const ExportGithub = ({
+  isVisible,
+  selectedRepo,
+  setSelectedRepo,
+  setError,
+  clearGitHubToken,
+  gitHubAccessToken,
+  setGitHubAccessToken,
+  setCanSendToGitHub,
+}: {
+  isVisible: boolean;
+  selectedRepo: string;
+  setSelectedRepo: (selectedRepo: string) => void;
+  setError: (error: string) => void;
+  clearGitHubToken: boolean;
+  gitHubAccessToken: string | null;
+  setGitHubAccessToken: (gitHubAccessToken: string | null) => void;
+  setCanSendToGitHub: (canSendToGitHub: boolean) => void;
+}) => {
+  const authWindow = useRef<Window | null>(null);
+
+  const [isGitHubAppInstalled, setIsGitHubAppInstalled] = useState(false);
+  const [clickedInstall, setClickedInstall] = useState(false);
+  const [repos, setRepos] = useState<UserRepos>(initialRepos);
+  const showGitHubLogin = isGitHubAppInstalled && !gitHubAccessToken;
+  const repoSelectEnabled = isGitHubAppInstalled && gitHubAccessToken;
+
+  const setRepo = (repo: string) => {
+    setSelectedRepo(repo);
+    localStorageSet("selected-repo", repo);
+  };
+
+  const clearAuth = () => {
+    setGitHubAccessToken(null);
+    localStorageSet("oauth-github", "");
+  };
+
+  const fetchAndSetInstallation = async () => {
+    try {
+      const res = await apiGet<{ installations: { app_id: number }[] }>({
+        domain: "https://api.github.com",
+        path: "user/installations",
+        headers: {
+          Authorization: `token ${gitHubAccessToken}`,
+        },
+      });
+      const installations = res.installations;
+      const isAppInstalled = installations.some(
+        (installation) => installation.app_id === 874256
+      );
+      setIsGitHubAppInstalled(isAppInstalled);
+      return isAppInstalled;
+    } catch (error) {
+      const e = error as Error;
+      if (e.message === "Bad credentials") {
+        clearAuth();
+      }
+      return false;
+    }
+  };
+
+  // listen for messages from the auth window
+  useEffect(() => {
+    const handleGitHubAuthMessage = (event: MessageEvent) => {
+      if (event.data) {
+        setGitHubAccessToken(event.data);
+        setClickedInstall(false);
+        localStorageSet("oauth-github", event.data);
+        authWindow.current?.close();
+      }
+    };
+
+    if (isVisible) {
+      window.addEventListener("message", handleGitHubAuthMessage);
+    }
+
+    return () => {
+      if (isVisible) {
+        window.removeEventListener("message", handleGitHubAuthMessage);
+      }
+    };
+  }, [isVisible]);
+
+  // check for installation
+  useEffect(() => {
+    if (gitHubAccessToken) fetchAndSetInstallation();
+  }, [gitHubAccessToken]);
+
+  // get the list of repos
+  useEffect(() => {
+    if (!gitHubAccessToken || !isGitHubAppInstalled) return;
+    const fetchAndSetRepos = async () => {
+      try {
+        const res = await apiGet<UserReposResponse>({
+          domain: "https://api.github.com",
+          path: "user/repos?per_page=100&type=owner",
+          headers: {
+            Authorization: `token ${gitHubAccessToken}`,
+          },
+        });
+        setError("");
+        setRepos(res.data);
+      } catch (error) {
+        setError("Failed to fetch repositories");
+      }
+    };
+    fetchAndSetRepos();
+  }, [gitHubAccessToken, isGitHubAppInstalled]);
+
+  // gatekeep export button
+  useEffect(() => {
+    if (gitHubAccessToken && isGitHubAppInstalled && selectedRepo) {
+      setCanSendToGitHub(true);
+    }
+  }, [gitHubAccessToken, isGitHubAppInstalled, selectedRepo]);
+
+  // clear github token if error writing to github
+  useEffect(() => {
+    if (clearGitHubToken) clearAuth();
+  }, [clearGitHubToken]);
+
+  const InstallButton = () => (
+    <Button
+      text="Install SamePage App"
+      id="qb-install-button"
+      icon="cloud-download"
+      className={clickedInstall ? "opacity-30 hover:opacity-100" : ""}
+      intent={clickedInstall ? "none" : "primary"}
+      onClick={async () => {
+        authWindow.current = window.open(
+          "https://github.com/apps/samepage-network",
+          "_blank",
+          `width=${WINDOW_WIDTH}, height=${WINDOW_HEIGHT}, top=${WINDOW_TOP}, left=${WINDOW_LEFT}`
+        );
+        setClickedInstall(true);
+        document.getElementById("qb-install-button")?.blur();
+      }}
+    />
+  );
+
+  const ConfirmInstallationButton = () => (
+    <Button
+      text="Confirm Installation"
+      icon="confirm"
+      intent="primary"
+      onClick={async () => {
+        setClickedInstall(false);
+        setIsGitHubAppInstalled(true);
+      }}
+    />
+  );
+
+  const AuthorizeButton = () => (
+    <Button
+      text="Authorize"
+      icon="key"
+      intent="primary"
+      onClick={async () => {
+        authWindow.current = window.open(
+          "https://github.com/login/oauth/authorize?client_id=Iv1.86a652fa1b0490df",
+          "_blank",
+          `width=${WINDOW_WIDTH}, height=${WINDOW_HEIGHT}, top=${WINDOW_TOP}, left=${WINDOW_LEFT}`
+        );
+      }}
+    />
+  );
+
+  if (!isVisible) return null;
+  return (
+    <div className="flex mb-4">
+      <div className="flex flex-col">
+        {!isGitHubAppInstalled && <InstallButton />}
+        {clickedInstall && <ConfirmInstallationButton />}
+      </div>
+      {showGitHubLogin && <AuthorizeButton />}
+      {repoSelectEnabled && (
+        <MenuItemSelect
+          items={repos.map((repo) => repo.full_name)}
+          onItemSelect={setRepo}
+          activeItem={selectedRepo}
+          filterable={true}
+          transformItem={(item) => item.split("/")[1]}
+          emptyValueText="Choose Repo"
+        />
+      )}
+    </div>
+  );
+};
