2326a56af6658f88fc16ce4ceca43028af2d3d14
Michael Gartner <mclicks@gmail.com>
2024-08-28 02:31:50 -0600

    get publish working

--- src/components/nanopub/Nanopub.tsx
+++ src/components/nanopub/Nanopub.tsx
@@ -1,6 +1,16 @@
 import React, { useEffect, useState, useMemo, useCallback } from "react";
 import { Dialog, Button } from "@blueprintjs/core";
-import init, { KeyPair, Nanopub, NpProfile, getNpServer } from "@nanopub/sign";
+
+// web.js:1014 Uncaught (in promise) TypeError: Failed to construct 'URL': Invalid URL
+//     at __wbg_init (web.js:1014:17)
+//     at Nanopub.tsx:200:7
+// import init, { KeyPair, Nanopub, NpProfile, getNpServer } from "@nanopub/sign";
+import init, {
+  Nanopub,
+  NpProfile,
+  getNpServer,
+  KeyPair,
+} from "https://unpkg.com/@nanopub/sign";
 import renderOverlay from "roamjs-components/util/renderOverlay";
 import { getNodeEnv } from "roamjs-components/util/env";
 import rdfString from "./exampleRdfString";
@@ -23,7 +33,7 @@ const SERVER_URL =
     : getNpServer(false);
 const PRIVATE_KEY =
   getNodeEnv() === "development"
-    ? "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCjY1gsFxmak6SOCouJPuEzHNForkqFhgfHE3aAIAx"
+    ? "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCjY1gsFxmak6SOCouJPuEzHNForkqFhgfHE3aAIAx+Y5q6UDEDM9Q0EksheNffJB4iPqsAfiFpY0ARQY92K5r8P4+a78eu9reYrb2WxZb1qPJmvR7XZ6sN1oHD7dd/EyQoJmQsmOKdrqaLRbzR7tZrf52yvKkwNWXcIVhW8uxe7iUgxiojZpW9srKoK/qFRpaUZSKn7Z/zgtDH9FJkYbBsGPDMqp78Kzt+sJb+U2W+wCSSy34jIUxx6QRbzvn6uexc/emFw/1DU5y7zBudhgC7mVk8vX1gUNKyjZBzlOmRcretrANgffqs5fx/TMHN1xtkA/H1u1IKBfKoyk/xThMLAgMBAAECggEAECuG0GZA3HF8OaqFgMG+W+agOvH04h4Pqv4cHjYNxnxpFcNV9nEssTKWSOvCwYy7hrwZBGV3PQzbjFmmrxVFs20+8yCD7KbyKKQZPVC0zf84bj6NTNgvr6DpGtDxINxuGaMjCt7enqhoRyRRuZ0fj2gD3Wqae/Ds8cpDCefkyMg0TvauHSUj244vGq5nt93txUv1Sa+/8tWZ77Dm0s5a3wUYB2IeAMl5WrO2GMvgzwH+zT+4kvNWg5S0Ze4KE+dG3lSIYZjo99h14LcQS9eALC/VBcAJ6pRXaCTT/TULtcLNeOpoc9Fu25f0yTsDt6Ga5ApliYkb7rDhV+OFrw1sYQKBgQDCE9so+dPg7qbp0cV+lbb7rrV43m5s9Klq0riS7u8m71oTwhmvm6gSLfjzqb8GLrmflCK4lKPDSTdwyvd+2SSmOXySw94zr1Pvc7sHdmMRyA7mH3m+zSOOgyCTTKyhDRCNcRIkysoL+DecDhNo4Fumf71tsqDYogfxpAQhn0re8wKBgQDXhMmmT2oXiMnYHhi2k7CJe3HUqkZgmW4W44SWqKHp0V6sjcHm0N0RT5Hz1BFFUd5Y0ZB3JLcah19myD1kKYCj7xz6oVLb8O7LeAZNlb0FsrtD7NU+Hciywo8qESiA7UYDkU6+hsmxaI01DsttMIdG4lSBbEjA7t4IQC5lyr7xiQKBgQCN87YGJ40Y5ZXCSgOZDepz9hqX2KGOIfnUv2HvXsIfiUwqTXs6HbD18xg3KL4myIBOvywSM+4ABYp+foY+Cpcq2btLIeZhiWjsKIrw71+Q/vIe0YDb1PGf6DsoYhmWBpdHzR9HN+hGjvwlsYny2L9Qbfhgxxmsuf7zeFLpQLijjwKBgH7TD28k8IOk5VKec2CNjKd600OYaA3UfCpP/OhDl/RmVtYoHWDcrBrRvkvEEd2/DZ8qw165Zl7gJs3vK+FTYvYVcfIzGPWA1KU7nkntwewmf3i7V8lT8ZTwVRsmObWU60ySJ8qKuwoBQodki2VX12NpMN1wgWe3qUUlr6gLJU4xAoGAet6nD3QKwk6TTmcGVfSWOzvpaDEzGkXjCLaxLKh9GreM/OE+h5aN2gUoFeQapG5rUwI/7Qq0xiLbRXw+OmfAoV2XKv7iI8DjdIh0F06mlEAwQ/B0CpbqkuuxphIbchtdcz/5ra233r3BMNIqBl3VDDVoJlgHPg9msOTRy13lFqc="
     : "";
 const ORCID = "https://orcid.org/0000-0000-0000-0000";
 const NAME = "Test User";
@@ -145,28 +155,110 @@ const NanopubDialog = ({
   //   fetchDiscourseContext();
   // }, [uid]);
 
-  const handlePublishIntro = async () => {
+  const handlePublish = async () => {
     try {
-      const profile = new NpProfile(PRIVATE_KEY, ORCID, NAME, "");
-      const np = new Nanopub(RDF_STR);
-      const result = await np.publish(profile, SERVER_URL);
+      const privateKey = `MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCjY1gsFxmak6SOCouJPuEzHNForkqFhgfHE3aAIAx+Y5q6UDEDM9Q0EksheNffJB4iPqsAfiFpY0ARQY92K5r8P4+a78eu9reYrb2WxZb1qPJmvR7XZ6sN1oHD7dd/EyQoJmQsmOKdrqaLRbzR7tZrf52yvKkwNWXcIVhW8uxe7iUgxiojZpW9srKoK/qFRpaUZSKn7Z/zgtDH9FJkYbBsGPDMqp78Kzt+sJb+U2W+wCSSy34jIUxx6QRbzvn6uexc/emFw/1DU5y7zBudhgC7mVk8vX1gUNKyjZBzlOmRcretrANgffqs5fx/TMHN1xtkA/H1u1IKBfKoyk/xThMLAgMBAAECggEAECuG0GZA3HF8OaqFgMG+W+agOvH04h4Pqv4cHjYNxnxpFcNV9nEssTKWSOvCwYy7hrwZBGV3PQzbjFmmrxVFs20+8yCD7KbyKKQZPVC0zf84bj6NTNgvr6DpGtDxINxuGaMjCt7enqhoRyRRuZ0fj2gD3Wqae/Ds8cpDCefkyMg0TvauHSUj244vGq5nt93txUv1Sa+/8tWZ77Dm0s5a3wUYB2IeAMl5WrO2GMvgzwH+zT+4kvNWg5S0Ze4KE+dG3lSIYZjo99h14LcQS9eALC/VBcAJ6pRXaCTT/TULtcLNeOpoc9Fu25f0yTsDt6Ga5ApliYkb7rDhV+OFrw1sYQKBgQDCE9so+dPg7qbp0cV+lbb7rrV43m5s9Klq0riS7u8m71oTwhmvm6gSLfjzqb8GLrmflCK4lKPDSTdwyvd+2SSmOXySw94zr1Pvc7sHdmMRyA7mH3m+zSOOgyCTTKyhDRCNcRIkysoL+DecDhNo4Fumf71tsqDYogfxpAQhn0re8wKBgQDXhMmmT2oXiMnYHhi2k7CJe3HUqkZgmW4W44SWqKHp0V6sjcHm0N0RT5Hz1BFFUd5Y0ZB3JLcah19myD1kKYCj7xz6oVLb8O7LeAZNlb0FsrtD7NU+Hciywo8qESiA7UYDkU6+hsmxaI01DsttMIdG4lSBbEjA7t4IQC5lyr7xiQKBgQCN87YGJ40Y5ZXCSgOZDepz9hqX2KGOIfnUv2HvXsIfiUwqTXs6HbD18xg3KL4myIBOvywSM+4ABYp+foY+Cpcq2btLIeZhiWjsKIrw71+Q/vIe0YDb1PGf6DsoYhmWBpdHzR9HN+hGjvwlsYny2L9Qbfhgxxmsuf7zeFLpQLijjwKBgH7TD28k8IOk5VKec2CNjKd600OYaA3UfCpP/OhDl/RmVtYoHWDcrBrRvkvEEd2/DZ8qw165Zl7gJs3vK+FTYvYVcfIzGPWA1KU7nkntwewmf3i7V8lT8ZTwVRsmObWU60ySJ8qKuwoBQodki2VX12NpMN1wgWe3qUUlr6gLJU4xAoGAet6nD3QKwk6TTmcGVfSWOzvpaDEzGkXjCLaxLKh9GreM/OE+h5aN2gUoFeQapG5rUwI/7Qq0xiLbRXw+OmfAoV2XKv7iI8DjdIh0F06mlEAwQ/B0CpbqkuuxphIbchtdcz/5ra233r3BMNIqBl3VDDVoJlgHPg9msOTRy13lFqc=`;
 
-      console.log("Nanopub:", np.info());
-      console.log("Published:", result.info());
+      const serverUrl = "";
+      const profile = new NpProfile(
+        privateKey,
+        "https://orcid.org/0000-0000-0000-0000",
+        "User Name",
+        ""
+      );
 
-      console.log("Info");
-      console.log("-----------------");
-      console.log("Private Key:", PRIVATE_KEY);
-      console.log("ORCID:", ORCID);
-      console.log("Name:", NAME);
-      console.log("Server URL:", SERVER_URL);
-      // console.log("RDF:", RDF_STR);
-      console.log("-----------------");
+      const np = await new Nanopub(RDF_STR).publish(profile, serverUrl);
+      console.log("Published info dict:", np.info());
+      // init().then(async () => {
+      //   const profile = new NpProfile(PRIVATE_KEY, ORCID, NAME, "");
+
+      //   const np = new Nanopub(RDF_STR);
+
+      //   const result = await np.publish(profile, SERVER_URL);
+
+      //   console.log("Nanopub:", np.info());
+      //   console.log("Published:", result.info());
+
+      //   console.log("Info");
+      //   console.log("-----------------");
+      //   console.log("Private Key:", PRIVATE_KEY);
+      //   console.log("ORCID:", ORCID);
+      //   console.log("Name:", NAME);
+      //   console.log("Server URL:", SERVER_URL);
+      //   // console.log("RDF:", RDF_STR);
+      //   console.log("-----------------");
+      // });
 
       // handleClose();
     } catch (error) {
       console.error("Error publishing nanopub:", error);
     }
+  };
+
+  // DEBUG
+  // DEBUG
+  // DEBUG
+  const [rdfOutput, setRdfOutput] = useState("");
+  const [checkedOutput, setCheckedOutput] = useState("");
+  const [signedOutput, setSignedOutput] = useState("");
+  const [publishedOutput, setPublishedOutput] = useState("");
+  const [publishedURL, setPublishedURL] = useState("");
+  const [keyPair, setKeyPair] = useState<KeyPair | null>(null);
+
+  useEffect(() => {
+    try {
+      // @ts-ignore
+      init().then(() => {
+        console.log("WASM Initialized");
+      });
+    } catch (error) {
+      console.error("Error initializing WASM:", error);
+    }
+  }, []);
+
+  const generateKeyPair = () => {
+    const keypair = new KeyPair().toJs();
+    setKeyPair(keypair);
+    console.log(keypair);
+  };
+
+  const checkNanopub = () => {
+    const np = new Nanopub(RDF_STR);
+    const checked = np.check();
+    console.log("Checked info dict:", checked.info());
+    console.log(checked);
+    setCheckedOutput(JSON.stringify(checked.info(), null, 2));
+  };
+
+  const signNanopub = () => {
+    const np = new Nanopub(RDF_STR);
+    console.log(np);
+    try {
+      console.log("signNanopub");
+      console.log(PRIVATE_KEY);
+      console.log(ORCID);
+      console.log(NAME);
+      const profile = new NpProfile(PRIVATE_KEY, ORCID, NAME, "");
+      console.log(profile);
+      const signed = np.sign(profile);
+      console.log("Signed info dict:", signed.info());
+      console.log(signed);
+      setSignedOutput(JSON.stringify(signed.info(), null, 2));
+    } catch (error) {
+      console.error("Error signing nanopub:", error);
+    }
+  };
+
+  const publishNanopub = () => {
+    const serverUrl = "";
+    const profile = new NpProfile(PRIVATE_KEY, ORCID, NAME, "");
+    const np = new Nanopub(RDF_STR);
+    np.publish(profile, serverUrl).then((published) => {
+      console.log("Published info dict:", published.info());
+      setPublishedOutput(JSON.stringify(published.info(), null, 2));
+      setRdfOutput(published.rdf());
+      setPublishedURL(published.info().published);
+    });
   };
 
   return (
@@ -178,7 +270,34 @@ const NanopubDialog = ({
       enforceFocus={false}
       className="w-full sm:w-full md:w-3/4 lg:w-full lg:max-w-5xl"
     >
-      {node ? (
+      <div className="bp3-dialog-body">
+        <p>
+          Key Pair:{" "}
+          {keyPair
+            ? `Public-${keyPair.public.length} Private-${keyPair.private.length}`
+            : "No output"}
+        </p>
+        <p>
+          Checked Output: {checkedOutput ? checkedOutput.length : "No output"}
+        </p>
+        <p>Signed Output: {signedOutput ? signedOutput.length : "No output"}</p>
+        <p>
+          Published Output:{" "}
+          {publishedOutput ? publishedOutput.length : "No output"}
+        </p>
+        <p>RDF Output: {rdfOutput ? rdfOutput.length : "No output"}</p>
+        <p>
+          Published URL:{" "}
+          {publishedURL ? (
+            <a href={publishedURL} target="_blank" rel="noopener noreferrer">
+              Link
+            </a>
+          ) : (
+            "No URL"
+          )}
+        </p>
+      </div>
+      {/* {node ? (
         <div className="bp3-dialog-body">
           <div className="space-y-4">
             <ContributorManager pageUid={uid} />
@@ -194,12 +313,21 @@ const NanopubDialog = ({
         </div>
       ) : (
         <p>No node found</p>
-      )}
+      )} */}
       <div className="bp3-dialog-footer">
         <div className="bp3-dialog-footer-actions">
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handlePublishIntro} intent={"primary"}>
-            Publish Intro
+          <Button onClick={generateKeyPair} intent={"primary"}>
+            Generate Key Pair
+          </Button>
+          <Button onClick={checkNanopub} intent={"primary"}>
+            Check
+          </Button>
+          <Button onClick={signNanopub} intent={"primary"}>
+            Sign
+          </Button>
+          <Button onClick={publishNanopub} intent={"primary"}>
+            Publish
           </Button>
         </div>
       </div>
